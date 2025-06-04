b46bba41cffc293bc5dced03fd65405a6ec20da8
schuetzda <david-schuetz@gmx.de>
2025-06-04 13:49:16 +0200

    Set static runtime off

--- premake5.lua
+++ premake5.lua
@@ -42,13 +42,11 @@ project "ImGui"
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++20"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++20"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
